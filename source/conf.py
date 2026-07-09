fad2aa3b26ecb7125be1fd1084add71f459d2fe3
David Laseca Perez <davidlaseca@eprosima.com>
2026-07-09 14:59:22 +0200

    Rename HTML title
    Signed-off-by: David Laseca Perez <davidlaseca@eprosima.com>

--- source/conf.py
+++ source/conf.py
@@ -140,7 +140,7 @@ html_sidebars = {
 # Fixed title for the HTML documentation.
 # If not set, it defaults to "<project> v<release> documentation".
 #
-html_title = 'Documentation Hub'
+html_title = 'eProsima Documentation Hub'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs. This file should be a Windows icon file (.ico) being 16x16 or 32x32
